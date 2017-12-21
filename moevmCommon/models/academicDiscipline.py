61218336ad8237be3156a4121227c8c96d035689
Ruslan Kadyrov <shtorm5945@yandex.ru>
2017-12-21 19:32:10 +0300

    new model for academicDiscipline

--- moevmCommon/models/academicDiscipline.py
+++ moevmCommon/models/academicDiscipline.py
@@ -1,59 +1,43 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.db import models
 from moevmCommon.models.userProfile import UserProfile
-#
-# class AcademicDiscipline(models.Model):
-#   name = models.CharField(max_length=150)
-#
-#   @staticmethod
-#   def create(**params):
-#     academicDiscipline = AcademicDiscipline.objects.create(
-#       name=params.get('name'),
-#     )
-#     academicDiscipline.save()
-#
-#     return academicDiscipline
-#
-#   def __str__(self):
-#     return self.name
-
 
 class AcademicDisciplineOfTeacher(models.Model):
-  user = models.ForeignKey(
-    UserProfile,
-    null=True
-  )
-  disc = models.CharField(
-    max_length=40,
-    null=True,
-  )
-  #Вид занятия
-  type = models.CharField(
-    max_length=40,
-    null=True,
-  )
-  characterUpdate = models.CharField(
-    max_length=250,
-    null=True,
-  )
-  completeMark = models.CharField(
-    max_length=250,
-    null=True,
-  )
-
-  @staticmethod
-  def create(**params):
-    academicDisciplineOfTeacher = AcademicDisciplineOfTeacher.objects.create(
-      user=params.get('user'),
-      disc=params.get('disc'),
-      type=params.get('type'),
-      characterUpdate=params.get('characterUpdate'),
-      completeMark=params.get('completeMark'),
+    user = models.ForeignKey(
+        UserProfile,
+        null=True
     )
-    academicDisciplineOfTeacher.save()
-
-    return academicDisciplineOfTeacher
-
-  def __str__(self):
-    return self.disc + ' ' + self.type
+    disc = models.CharField(
+        max_length=40,
+        null=True,
+    )
+    type = models.CharField(
+        max_length=40,
+        null=True,
+    )
+    characterUpdate = models.CharField(
+        max_length=250,
+        null=True,
+    )
+    completeMark = models.CharField(
+        max_length=250,
+        null=True,
+    )
+    year = models.CharField(
+        max_length=4,
+        null=False
+    )
+    @staticmethod
+    def create(**params):
+        academicDisciplineOfTeacher = AcademicDisciplineOfTeacher.objects.create(
+            user=params.get('user'),
+            disc=params.get('disc'),
+            type=params.get('type'),
+            characterUpdate=params.get('characterUpdate'),
+            completeMark=params.get('completeMark'),
+            year=params.get('year')
+        )
+        academicDisciplineOfTeacher.save()
+        return academicDisciplineOfTeacher
+    def __str__(self):
+        return self.disc + ' ' + self.type
