69e4cce08683ecd1b773f83ad96e3aa1ec8aa187
Ruslan Kadyrov <shtorm5945@yandex.ru>
2017-12-21 19:35:18 +0300

    new model for userProfile

--- moevmCommon/models/userProfile.py
+++ moevmCommon/models/userProfile.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
@@ -7,133 +6,113 @@ PERSON_TYPE_CHOICES = (
     ('t', 'Преподаватель'),
     ('a', 'Администратор'),
 )
-ACADEMIC_STATUS_CHOICES  = (
-  ('a','Ассистент'),
-  ('s','Старший преподаватель'),
-  ('d','Доцент'),
-  ('p','Профессор'),
+ACADEMIC_STATUS_CHOICES = (
+    ('a', 'Ассистент'),
+    ('s', 'Старший преподаватель'),
+    ('d', 'Доцент'),
+    ('p', 'Профессор'),
 )
-
 ACADEMIC_DEGREE_CHOICES = (
-  ('n','Без степени'),
-  ('t','Кандидат наук'),
-  ('d','Доктор наук'),
+    ('n', 'Без степени'),
+    ('t', 'Кандидат наук'),
+    ('d', 'Доктор наук'),
 )
-
 class UserProfile(models.Model):
-  user = models.OneToOneField(User)
-
-  patronymic = models.CharField(
-    max_length=30,
-    null=True,
-  )
-  birth_date = models.DateField(
-    null=True,
-  )
-  github_id = models.CharField(
-    max_length=100,
-    null=True,
-  )
-  stepic_id = models.CharField(
-    max_length=100,
-    null=True,
-  )
-  type = models.CharField(
-    max_length=1,
-    choices=PERSON_TYPE_CHOICES,
-    null=True
-  )
-  # Дата текущего избрания или зачисления на преподавательскую должность
-  election_date = models.DateField(null=True)
-
-  # Срок окончания трудового договора
-  contract_date = models.DateField(null=True)
-
-  # Ученая степень
-  academic_degree = models.CharField(
-    max_length=1,
-    choices=ACADEMIC_DEGREE_CHOICES,
-    null=True
-  )
-
-  # Год присвоения ученой степени
-  year_of_academic_degree = models.DateField(null=True)
-
-  # Учебное звание
-  academic_status = models.CharField(
-    max_length=1,
-    choices=ACADEMIC_STATUS_CHOICES,
-    null=True
-  )
-  year_of_academic_status = models.DateField(null=True)
-
-  @property
-  def first_name(self):
-    return self.user.first_name
-
-  @property
-  def last_name(self):
-    return self.user.last_name
-
-  @property
-  def login(self):
-    return self.user.username
-
-  @property
-  def password(self):
-    return self.user.password
-
-  @property
-  def email(self):
-    return self.user.email
-
-  @staticmethod
-  def create(login, password, email, **params):
-    user = User.objects.create_user(login, password, email)
-    user.first_name = params.get('first_name')
-    user.last_name = params.get('last_name')
-    user.save()
-
-    user_profile = UserProfile.objects.create(
-      user=user,
-      patronymic=params.get('patronymic'),
-      birth_date=params.get('birth_date'),
-      github_id=params.get('github_id'),
-      stepic_id=params.get('stepic_id'),
-      type=params.get('type'),
-      election_date=params.get('election_date'),
-      contract_date=params.get('contract_date'),
-      academic_degree=params.get('academic_degree'),
-      year_of_academic_degree=params.get('year_of_academic_degree'),
-      academic_status=params.get('academic_status'),
-      year_of_academic_status=params.get('year_of_academic_status')
+    user = models.OneToOneField(User)
+    patronymic = models.CharField(
+        max_length=30,
+        null=True,
     )
-
-    user_profile.save()
-
-    return user_profile
-
-  @staticmethod
-  def create_of_user(**params):
-    user_profile = UserProfile.objects.create(
-      user=params.get('user'),
-      patronymic=params.get('patronymic'),
-      birth_date=params.get('birth_date'),
-      github_id=params.get('github_id'),
-      stepic_id=params.get('stepic_id'),
-      type=params.get('type'),
-      election_date=params.get('election_date'),
-      contract_date=params.get('contract_date'),
-      academic_degree=params.get('academic_degree'),
-      year_of_academic_degree=params.get('year_of_academic_degree'),
-      academic_status=params.get('academic_status'),
-      year_of_academic_status=params.get('year_of_academic_status')
+    birth_date = models.DateField(
+        null=True,
     )
-
-    return user_profile
-
-  def __str__(self):
-    return self.first_name + ' ' + self.last_name + ' ' + self.patronymic
-
-  class Meta:
-    db_table = 'userprofiles'
+    github_id = models.CharField(
+        max_length=100,
+        null=True,
+    )
+    stepic_id = models.CharField(
+        max_length=100,
+        null=True,
+    )
+    type = models.CharField(
+        max_length=1,
+        choices=PERSON_TYPE_CHOICES,
+        null=True
+    )
+    # Дата текущего избрания или зачисления на преподавательскую должность
+    election_date = models.DateField(null=True)
+    # Срок окончания трудового договора
+    contract_date = models.DateField(null=True)
+    # Ученая степень
+    academic_degree = models.CharField(
+        max_length=1,
+        choices=ACADEMIC_DEGREE_CHOICES,
+        null=True
+    )
+    # Год присвоения ученой степени
+    year_of_academic_degree = models.DateField(null=True)
+    # Учебное звание
+    academic_status = models.CharField(
+        max_length=1,
+        choices=ACADEMIC_STATUS_CHOICES,
+        null=True
+    )
+    year_of_academic_status = models.DateField(null=True)
+    @property
+    def first_name(self):
+        return self.user.first_name
+    @property
+    def last_name(self):
+        return self.user.last_name
+    @property
+    def login(self):
+        return self.user.username
+    @property
+    def password(self):
+        return self.user.password
+    @property
+    def email(self):
+        return self.user.email
+    @staticmethod
+    def create(login, password, email, **params):
+        user = User.objects.create_user(login, password, email)
+        user.first_name = params.get('first_name')
+        user.last_name = params.get('last_name')
+        user.save()
+        user_profile = UserProfile.objects.create(
+            user=user,
+            patronymic=params.get('patronymic'),
+            birth_date=params.get('birth_date'),
+            github_id=params.get('github_id'),
+            stepic_id=params.get('stepic_id'),
+            type=params.get('type'),
+            election_date=params.get('election_date'),
+            contract_date=params.get('contract_date'),
+            academic_degree=params.get('academic_degree'),
+            year_of_academic_degree=params.get('year_of_academic_degree'),
+            academic_status=params.get('academic_status'),
+            year_of_academic_status=params.get('year_of_academic_status')
+        )
+        user_profile.save()
+        return user_profile
+    @staticmethod
+    def create_of_user(**params):
+        user_profile = UserProfile.objects.create(
+            user=params.get('user'),
+            patronymic=params.get('patronymic'),
+            birth_date=params.get('birth_date'),
+            github_id=params.get('github_id'),
+            stepic_id=params.get('stepic_id'),
+            type=params.get('type'),
+            election_date=params.get('election_date'),
+            contract_date=params.get('contract_date'),
+            academic_degree=params.get('academic_degree'),
+            year_of_academic_degree=params.get('year_of_academic_degree'),
+            academic_status=params.get('academic_status'),
+            year_of_academic_status=params.get('year_of_academic_status')
+        )
+        return user_profile
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name + ' ' + self.patronymic
+    class Meta:
+        db_table = 'userprofiles'
