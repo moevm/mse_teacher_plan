7685af25df836f2481fd24ca290803b41c84b57f
Ruslan Kadyrov <shtorm5945@yandex.ru>
2017-12-21 19:33:08 +0300

    new model for nir

--- moevmCommon/models/nir.py
+++ moevmCommon/models/nir.py
@@ -1,42 +1,41 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.db import models
 from moevmCommon.models.userProfile import UserProfile
 
 class NIR(models.Model):
-  user = models.ForeignKey(UserProfile)
-  workName = models.CharField(max_length=250)
-  startDate = models.DateField(null=True)
-  finishDate = models.DateField(null=True)
-  role = models.CharField(
-    max_length=100,
-    null=True
-  )
-  organisation = models.CharField(
-    max_length=250,
-    null=True
-  )
-  # Шифр
-  cipher = models.CharField(
-    max_length="100",
-    null=True
-  )
-
-  @staticmethod
-  def create(**params):
-    nir = NIR.objects.create(
-      user=params.get('user'),
-      workName=params.get('workName'),
-      startDate=params.get('startDate'),
-      finishDate = params.get('finishDate'),
-      role = params.get('role'),
-      organisation = params.get('organisation'),
-      cipher = params.get('cipher'),
+    user = models.ForeignKey(UserProfile)
+    workName = models.CharField(max_length=250)
+    startDate = models.DateField(null=True)
+    finishDate = models.DateField(null=True)
+    role = models.CharField(
+        max_length=100,
+        null=True
     )
-
-    nir.save()
-
-    return nir
-
-  def __str__(self):
-    return self.workName + " " + self.organisation + " " + self.cipher
+    organisation = models.CharField(
+        max_length=250,
+        null=True
+    )
+    cipher = models.CharField(
+        max_length=100,
+        null=True
+    )
+    year = models.CharField(
+        max_length=4,
+        null=False
+    )
+    @staticmethod
+    def create(**params):
+        nir = NIR.objects.create(
+            user=params.get('user'),
+            workName=params.get('workName'),
+            startDate=params.get('startDate'),
+            finishDate=params.get('finishDate'),
+            role=params.get('role'),
+            organisation=params.get('organisation'),
+            cipher=params.get('cipher'),
+            year=params.get('year')
+        )
+        nir.save()
+        return nir
+    def __str__(self):
+        return self.workName + " " + self.organisation + " " + self.cipher
