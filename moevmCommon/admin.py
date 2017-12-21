fd11c04ba17dd24b9ff4d454faf88561ca02da56
Ruslan Kadyrov <shtorm5945@yandex.ru>
2017-12-21 19:37:52 +0300

    admin fixed

--- moevmCommon/admin.py
+++ moevmCommon/admin.py
@@ -6,6 +6,9 @@ from moevmCommon.models.academicDiscipline import *
 from moevmCommon.models.nir import *
 from moevmCommon.models.publication import *
 from moevmCommon.models.scientificEvent import *
+from moevmCommon.models.books import *
+from moevmCommon.models.qualification import *
+from moevmCommon.models.other import *
 
 admin.site.register(UserProfile)
 # admin.site.register(AcademicDiscipline)
@@ -14,3 +17,6 @@ admin.site.register(NIR)
 admin.site.register(ScientificEvent)
 # admin.site.register(Participation)
 admin.site.register(Publication)
+admin.site.register(Book)
+admin.site.register(Qualification)
+admin.site.register(Other)
